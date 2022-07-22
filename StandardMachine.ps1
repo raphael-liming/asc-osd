4de6c1138546c63bab9ea564b125ec681e29e663
raphael-liming <raphael.liming@outlook.com>
2022-07-22 13:03:51 -0400

    Add files via upload

---
+++ StandardMachine.ps1
@@ -0,0 +1,27 @@
+Write-Host  -ForegroundColor Cyan "Starting American Safety Council's Custom OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+# sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
+Install-Module OSD -Force
+Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
+Import-Module OSD -Force
+
+# Start OSDCloud ZTI
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with Pre-Defined Values"
+Start-OSDCloud -Screenshot -OSLanguage en-us -OSBuild 21H1 -OSEdition Pro -ZTI
+
+# Post Actions
+# Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+# Write-Warning "I'm not sure of what to put here yet"
+
+# Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
